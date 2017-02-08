d59f5f46451e1cbe97534a138039aac44a8af2fa
Gustavo Espindola <yhoazk@gmail.com>
2017-02-08 14:48:40 -0600

    Need to handle case when the value is 0

--- test_images/test_threshold.py
+++ test_images/test_threshold.py
@@ -19,6 +19,20 @@ def get_yellow(img):
     mask = cv2.inRange(img_hsv, (20,50,150),(40,255,255))
     return mask
 
+def remove_outliers(data, m=2):
+    mean = np.mean(data)
+    print(mean)
+    std_data = m*np.std(data)
+    print(std_data)
+    data = list(data)
+    print(data)
+    #ret_data = np.array(filter(lambda x: abs(x-mean) < std_data, data))
+    #ret_data = [d for d in data if (abs(d-mean) < std_data) else mean]
+    ret_data = [d  if (abs(d-mean) < std_data or d == 0) else mean for d in data]
+    print(ret_data)
+    return ret_data 
+
+
 def birds_eye_transform(img):
     # from the center of the image
     print(img.shape)
@@ -55,18 +69,29 @@ def get_hist_slice(img, slices=10):
     location_r = []
     location_ry = []
     location_ly = []
-    location = {'l':location_l, 'r':location_r, 'ly':location_ly, 'ry':location_ry}
     for window in range(0,h_img, int(h_img/ slices)):
         sli = img[window:int(window+(h_img/slices)), :]
         sli_sum = np.sum(sli, axis=0)  # get the sum from all the columns
         sli_l, sli_r = (sli_sum[:w_img], sli_sum[w_img:])
         l_indx = np.mean(np.argpartition(sli_l, -5)[-5:])
         r_indx = np.mean(np.argpartition(sli_r, -5)[-5:]) + w_img
-
+        # add condtion for the case when the index is 0
         location_ly.append(window)
         location_ry.append(window)
         location_l.append(l_indx)
         location_r.append(r_indx)
+    # if a point is 0 make its value the median btw the point before and the point after
+    for n,x in enumerate(location_l):
+        if x == 0:
+            location_ly.pop(n)
+            print( location_l.pop(n))
+
+    for n,x in enumerate(location_r):
+        if x == 0:
+            location_ry.pop(n)
+            location_r.pop(n)
+
+    location = {'l':location_l, 'r':location_r, 'ly':location_ly, 'ry':location_ry}
     return location
 
 files = glob("./*.jpg")
@@ -79,18 +104,23 @@ for i in files:
     #hist = np.sum(img[img.shape[0]/4:,:], axis=0)
     # b = th_image(b)
     ##### Find the lane fitting
-    fit_l = np.polyfit(lane_pts['ly'], lane_pts['l'], deg=3)
+    fit_l = np.polyfit(lane_pts['ly'], remove_outliers(lane_pts['l'], 1.2), deg=2)
+    fit_r = np.polyfit(lane_pts['ry'], remove_outliers(lane_pts['r'], 1.2), deg=2)
     poly_line_l = np.poly1d(fit_l)
+    poly_line_r = np.poly1d(fit_r)
     print(fit_l)
-    line_lsp = np.linspace(0,720-1, 720)
+    line_lsp = np.linspace(0,720-1, 100)
     #line_l = fit_l[0]*fit_l[0]*fit_l[0]*line_lsp + fit_l[1]*fit_l[1]*line_lsp + fit_l[2]*line_lsp + lane_pts['l'][0]
     line_l = poly_line_l(line_lsp)
+    line_r = poly_line_r(line_lsp)
     #####
 
-    f, (ax1, ax2) = plt.subplots(1,2)
-    ax1.plot(lane_pts['ly'], lane_pts['l'])
-    ax2.imshow(b, cmap='gray')
-    ax2.plot(line_l, line_lsp)
-    ax2.scatter(lane_pts['l'], lane_pts['ly'], s=50, c='red', marker='o')
+   # f, (ax1, ax2) = plt.subplots(1,2)
+   # ax1.plot(lane_pts['ly'], lane_pts['l'])
+    plt.imshow(b, cmap='gray')
+    plt.plot(line_l, line_lsp, 'g^')
+    plt.plot(line_r, line_lsp, 'r^')
+    plt.scatter(lane_pts['l'], lane_pts['ly'], s=50, c='red', marker='o')
+    plt.scatter(lane_pts['r'], lane_pts['ry'], s=50, c='red', marker='o')
     #plt.imshow(th_image(img))
     plt.show()
