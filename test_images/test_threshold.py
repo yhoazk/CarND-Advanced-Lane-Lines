1f306df0a394753c29022c93b0dc390481bdb62c
porko <yhoazk@gmail.com>
2017-02-09 04:13:22 -0600

    fix

--- test_images/test_threshold.py
+++ test_images/test_threshold.py
@@ -19,8 +19,8 @@ def get_yellow(img):
     mask = cv2.inRange(img_hsv, (20,50,150),(40,255,255))
     return mask
 
-def remove_outliers(data_x,data_y, m=2):
-    mean = np.mean(data_x)
+def remove_outliers(data, m=2):
+    mean = np.mean(data)
     #print(mean)
     std_data = m*np.std(data)
     #print(std_data)
@@ -28,7 +28,7 @@ def remove_outliers(data_x,data_y, m=2):
     #print(data)
     #ret_data = np.array(filter(lambda x: abs(x-mean) < std_data, data))
     #ret_data = [d for d in data if (abs(d-mean) < std_data) else mean]
-    ret_data = [d  if (abs(d-mean) < std_data or d == 0) else mean for d in data]
+    ret_data = [d  if (abs(d-mean) < std_data) else mean for d in data]
     #print(ret_data)
     return ret_data
 
@@ -69,7 +69,7 @@ def get_hist_slice(img, slices=10):
     location_r = []
     location_ry = []
     location_ly = []
-    for window in range(0,h_img, int(h_img/ slices)):
+    for window in reversed(range(0,h_img, int(h_img/ slices))):
         sli = img[window:int(window+(h_img/slices)), :]
         sli_sum = np.sum(sli, axis=0)  # get the sum from all the columns
         sli_l, sli_r = (sli_sum[:w_img], sli_sum[w_img:])
@@ -93,18 +93,6 @@ def get_hist_slice(img, slices=10):
         #print("r_indx: " + str(r_indx) + " sli_r: " + str(sli_r))
         # add condtion for the case when the index is 0
     # if a point is 0 make its value the median btw the point before and the point after
-    """
-
-    for n,x in enumerate(location_l):
-        if x == 0:
-            location_ly.pop(n)
-            print( location_l.pop(n))
-
-    for n,x in enumerate(location_r):
-        if x == 0:
-            location_ry.pop(n)
-            location_r.pop(n)
-    """
     location = {'l':location_l, 'r':location_r, 'ly':location_ly, 'ry':location_ry}
     print("l : " + str(len(location_l)))
     print(location_l)
@@ -126,16 +114,27 @@ for i in files:
     #fit_l = np.polyfit(lane_pts['ly'], remove_outliers(lane_pts['l'], 1.2), deg=2)
     #fit_r = np.polyfit(lane_pts['ry'], remove_outliers(lane_pts['r'], 1.2), deg=2)
 
-    fit_l = np.polyfit(lane_pts['ly'], lane_pts['l'], deg=2)
-    fit_r = np.polyfit(lane_pts['ry'], lane_pts['r'], deg=2)
+    fit_l, v_l = np.polyfit(lane_pts['ly'], remove_outliers(lane_pts['l']), deg=2,cov=True)
+    print("---v_l---")
+    print(v_l)
+    error_l = np.sum(np.abs(v_l[:][:][2]))
 
+    fit_r, v_r = np.polyfit(lane_pts['ry'], remove_outliers(lane_pts['r']), deg=2,cov=True)
+    print("---v_r----")
+    print(v_r)
+    error_r =np.sum(np.abs(v_r[:][:][2]))
+    print("------------")
     poly_line_l = np.poly1d(fit_l)
     poly_line_r = np.poly1d(fit_r)
-    print(fit_l)
+    print("error_r: " + str(error_r))
+    print("error_l: " + str(error_l))
     line_lsp = np.linspace(0,720-1, 100)
     #line_l = fit_l[0]*fit_l[0]*fit_l[0]*line_lsp + fit_l[1]*fit_l[1]*line_lsp + fit_l[2]*line_lsp + lane_pts['l'][0]
     line_l = poly_line_l(line_lsp)
     line_r = poly_line_r(line_lsp)
+    print("-----------------")
+    print(fit_r)
+    print(fit_l)
     #####
 
    # f, (ax1, ax2) = plt.subplots(1,2)
