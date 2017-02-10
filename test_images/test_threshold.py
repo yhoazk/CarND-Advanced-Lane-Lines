144bac43c08eced4ba95b7027be5f932c1794872
Gustavo Espindola <yhoazk@gmail.com>
2017-02-10 17:19:27 -0600

    Mask added to hist function

--- test_images/test_threshold.py
+++ test_images/test_threshold.py
@@ -4,7 +4,7 @@ import cv2
 import matplotlib.pyplot as plt
 from glob import glob
 import numpy as np
-from moviepy.video.io.VideoFileClip import VideoFileClip
+#from moviepy.video.io.VideoFileClip import VideoFileClip
 # define color boundaries
 boundaries = [
     ([17, 15, 100], [50, 56, 200]),
@@ -19,18 +19,23 @@ def get_yellow(img):
     mask = cv2.inRange(img_hsv, (20,50,150),(40,255,255))
     return mask
 
-def remove_outliers(data, m=2):
-    mean = np.mean(data)
+def remove_outliers(data_x, data_y, m=2):
+    mean = np.mean(data_x)
     #print(mean)
-    std_data = m*np.std(data)
+    std_data = m*np.std(data_x)
     #print(std_data)
-    data = list(data)
-    #print(data)
-    #ret_data = np.array(filter(lambda x: abs(x-mean) < std_data, data))
     #ret_data = [d for d in data if (abs(d-mean) < std_data) else mean]
-    ret_data = [d  if (abs(d-mean) < std_data) else mean for d in data]
+    #ret_data = [d  if (abs(d-mean) < std_data) else mean for d in data]
+    ret_data_y = []
+    ret_data_x = []
+    for x,y in zip(data_x, data_y):
+        if (abs(x-mean) < std_data):
+            ret_data_x.append(x)
+            ret_data_y.append(y)
+
+
     #print(ret_data)
-    return ret_data
+    return (ret_data_y, ret_data_x)
 
 
 def birds_eye_transform(img):
@@ -65,30 +70,50 @@ def rev_birds_eye_transform(img):
    # bird_img = bird_img[::-1]
     return  warp_img
 
-def th_image(img):
-    img_g = np.uint8(cv2.cvtColor(img, cv2.COLOR_BGR2HLS))
-    s_img = img_g[:,:,2]
-    th_img = np.zeros_like(s_img)
-    th_img[(s_img > 210)&(s_img<=255)] = 1
-    edge = cv2.Canny(s_img,250,200)
-    #sbl_img = np.abs(cv2.Sobel(th_img, cv2.CV_64F, 1,0))
-    return edge
-
-def get_hist_slice(img, slices=10):
+def get_hist_slice(img, slices=10, margin=100):
     """
     Returns the possible location of the center of the line
     based on the pixels with val = 1
+    The image received has to be binarized.
     """
     h_img = img.shape[0]
-    w_img = int(img.shape[1]/2)
+    w_img = img.shape[1]
     location_l = []
     location_r = []
     location_ry = []
     location_ly = []
+
+    """
+    Createa a mask to ignore the center and the extremes of the image.
+    ****-----***-----****
+    ****-----***-----****
+    ****-----***-----****
+    ****-----***-----****
+    ****-----***-----****
+    ****-----***-----****
+    ****-----***-----****
+    """
+    zero_patch = np.zeros((h_img, margin))
+    one_patch  = np.ones((h_img, (w_img//2)-(1.5*margin)))
+
+    mask = np.c_[zero_patch, one_patch]
+    mask = np.c_[mask, zero_patch]
+    mask = np.c_[mask, one_patch]
+    mask = np.c_[mask, zero_patch]
+    img = np.uint8(img)
+    mask = np.uint8(mask)
+    print("img:"+str(img.shape))
+    print("mask:"+str(mask.shape))
+    img = cv2.bitwise_and(img,img,mask = mask)
+#    plt.imshow(img)
+#    plt.show()
     for window in reversed(range(0,h_img, int(h_img/ slices))):
         sli = img[window:int(window+(h_img/slices)), :]
         sli_sum = np.sum(sli, axis=0)  # get the sum from all the columns
-        sli_l, sli_r = (sli_sum[:w_img], sli_sum[w_img:])
+        """
+        Add a margin to the histogram to not take pixels at the far left or right
+        """
+        sli_l, sli_r = (sli_sum[:w_img//2], sli_sum[w_img//2:]) # TODO: change 1280 to a w_img
 
         # get the location of 5 top max elements
         l_arg = np.argpartition(sli_l, -5)[-5:]
@@ -103,7 +128,7 @@ def get_hist_slice(img, slices=10):
             location_ly.append(window)
 
         if mag_r > 100:
-            r_indx = np.mean(r_arg) + w_img
+            r_indx = np.mean(r_arg) + w_img//2
             location_ry.append(window)
             location_r.append(r_indx)
         #print("r_indx: " + str(r_indx) + " sli_r: " + str(sli_r))
@@ -112,34 +137,57 @@ def get_hist_slice(img, slices=10):
     location = {'l':location_l, 'r':location_r, 'ly':location_ly, 'ry':location_ry}
     print("l : " + str(len(location_l)))
     print(location_l)
+    print("ly : " + str(len(location_ly)))
+    print(location_ly)
+
     print("r : " + str(len(location_r)))
     print(location_r)
+    print("ry : " + str(len(location_ry)))
+    print(location_ry)
+
 
     return location
 
 
 def get_lane_area(poly_l, poly_r, n=10):
     """
-    Create a list of points that describe the 
+    Create a list of points that describe the
     area between the lanes, starts from the bottom
     left corner
     """
     # set the list of points as needed by polyfill
     x = np.linspace(0, 720, n)
-    fy_l = poly_l(x) 
-    fy_r = poly_r(x) 
+    fy_l = poly_l(x)
+    fy_r = poly_r(x)
 
     return np.append(np.c_[fy_l,x], np.c_[fy_r,x][::-1], axis=0)
-    
+
 def process_lane(img):
     img_th = th_image(img)
     b_img = birds_eye_transform(img_th)
+    plt.imshow(b_img)
     lane_pts = get_hist_slice(b_img)
-    fit_l, v_l = np.polyfit(lane_pts['ly'], remove_outliers(lane_pts['l']), deg=2,cov=True)
-    fit_r, v_r = np.polyfit(lane_pts['ry'], remove_outliers(lane_pts['r']), deg=2,cov=True)
+    #fit_l, v_l = np.polyfit(*remove_outliers(lane_pts['ly'], lane_pts['l']), deg=2,cov=True)
+    #fit_r, v_r = np.polyfit(*remove_outliers(lane_pts['ry'], lane_pts['r']), deg=2,cov=True)
+    plt.scatter(lane_pts['l'], lane_pts['ly'], s=50, c='red', marker='o')
+    plt.scatter(lane_pts['r'], lane_pts['ry'], s=50, c='red', marker='o')
+    try:
+        fit_l, v_l = np.polyfit(*remove_outliers(lane_pts['l'], lane_pts['ly']), deg=2,cov=True)
+    except:
+        fit_l = np.polyfit(*remove_outliers(lane_pts['l'], lane_pts['ly']), deg=2)
+
+# The covariance matrix yields to exceptions in certain scenarios, then is not a reliable estimator
+    try:
+        fit_r, v_r = np.polyfit(*remove_outliers(lane_pts['r'], lane_pts['ry']), deg=2,cov=True)
+    except:
+        fit_r = np.polyfit(*remove_outliers(lane_pts['r'], lane_pts['ry']), deg=2)
     poly_line_l = np.poly1d(fit_l)
     poly_line_r = np.poly1d(fit_r)
+    plt.plot(poly_line_l(lane_pts['ly']), lane_pts['ly'], 'g^')
+    plt.plot(poly_line_r(lane_pts['ry']), lane_pts['ry'], 'r^')
+    plt.show()
     shade_polygon = np.int32(get_lane_area(poly_line_l, poly_line_r))
+
     shade = np.zeros_like(img_th)
     shade = cv2.fillConvexPoly(shade,shade_polygon,1)
     warp = rev_birds_eye_transform(shade)
@@ -147,13 +195,34 @@ def process_lane(img):
     shaded_lane= cv2.addWeighted(img, 0.6, warp_3, 0.4,0)
     return shaded_lane
 
+
+def th_image(img):
+    img_g = np.uint8(cv2.cvtColor(img, cv2.COLOR_BGR2HLS))
+    s_img = img_g[:,:,2]
+    print(s_img.shape)
+    ret, th = cv2.threshold(s_img,127,255,cv2.THRESH_BINARY)
+    #th_img = np.zeros_like(s_img)
+    #th_img[(s_img > 50)] = 1
+#    edge = cv2.Canny(s_img,250,200)
+    #sbl_img = np.abs(cv2.Sobel(th_img, cv2.CV_64F, 0,1))
+    return th
+
+
+files = "test5.jpg"# glob("./*.jpg")
 files = glob("./*.jpg")
+#files = "test_0044.jpg"# glob("./*.jpg")
+#files = glob("./*.jpg")
 for i in files:
     img = plt.imread(i)
     img = process_lane(img)
+    
+#    cv2.imshow("h",img)
+#    cv2.waitKey(400)
+ #   cv2.destroyAllWindows()
 
-    plt.imshow(img)
-    plt.show()
+    #clip = VideoFileClip("../project_video.mp4")
+    #clip = clip.fl_image(process_lane)
+    #clip.write_videofile("./out_video.mp4", audio=False)
 """
     img_th = th_image(img)
     _,b = birds_eye_transform(img_th)
